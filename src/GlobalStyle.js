29f1e470b69262bdd4ee2f760d68875cdc6ef2a1
Krystian Żybura <121553175+KrystianZybura@users.noreply.github.com>
2023-11-22 22:20:37 +0100

    initial styles added

---
+++ src/GlobalStyle.js
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from "styled-components";
+
+const styled = { createGlobalStyle };
+
+export const GlobalStyle = styled.createGlobalStyle`
+  html {
+    *,
+    *::before,
+    *::after {
+      box-sizing: border-box;
+    }
+    font-family: "Poppins", sans-serif;
+  }
+
+  body {
+    height: 100vh;
+    background: ${({ theme }) => theme.lightGrey};
+  }
+`;
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "./GlobalStyle.js";
+import { Normalize } from "styled-normalize";
+import { theme } from "./theme.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <Normalize />
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
