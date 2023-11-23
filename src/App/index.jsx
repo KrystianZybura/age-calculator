911a383ca617ed7fb7f33c1c5d0b8a29a0a7262b
Krystian Żybura <Krystian12324444@gmail.com>
2023-11-23 20:13:50 +0100

    refactored

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -8,12 +8,10 @@ function App() {
           Day
           <StyledInput name="day" type="number" placeholder="DD" autoFocus />
         </StyledLabel>
-
         <StyledLabel htmlFor="month">
           Month
           <StyledInput name="month" type="number" placeholder="MM" />
         </StyledLabel>
-
         <StyledLabel htmlFor="year">
           Year
           <StyledInput name="year" type="number" placeholder="YYYY" />
