86d35860b8787a8a61cc9c8c4a4cbf32eb0f5414
Krystian Żybura <Krystian12324444@gmail.com>
2023-11-23 14:00:49 +0100

    inputs design implemented

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,12 +1,23 @@
-import { Form, Input, InputsWrapper } from "./styled";
+import { Form, InputsWrapper, StyledInput, StyledLabel } from "./styled";
 
 function App() {
   return (
     <Form>
       <InputsWrapper>
-        <Input />
-        <Input />
-        <Input />
+        <StyledLabel htmlFor="day">
+          Day
+          <StyledInput name="day" type="number" placeholder="DD" autoFocus />
+        </StyledLabel>
+
+        <StyledLabel htmlFor="month">
+          Month
+          <StyledInput name="month" type="number" placeholder="MM" />
+        </StyledLabel>
+
+        <StyledLabel htmlFor="year">
+          Year
+          <StyledInput name="year" type="number" placeholder="YYYY" />
+        </StyledLabel>
       </InputsWrapper>
     </Form>
   );
--- src/App/styled.js
+++ src/App/styled.js
@@ -2,16 +2,49 @@ import styled from "styled-components";
 
 export const Form = styled.form`
   background: ${({ theme }) => theme.white};
+  padding: 40px;
+  border-radius: 20px 20px 60px 20px;
+  color: ${({ theme }) => theme.offBlack};
 `;
 
 export const InputsWrapper = styled.div`
   display: grid;
-  grid-template-columns: repeat(3, 1fr);
+  grid-template-columns: repeat(3, 150px);
+  grid-gap: 40px;
 `;
 
-export const Input = styled.input`
-  padding: 20px;
-  border-radius: 10px;
+export const StyledInput = styled.input`
+  padding: 17px;
+  border-radius: 7px;
   font-weight: bold;
-  font-size: 20px;
+  font-size: 23px;
+  border: 1px solid ${({ theme }) => theme.lightGrey};
+  appearance: "textfield";
+
+  &[type="number"] {
+    -moz-appearance: textfield;
+    appearance: textfield;
+    margin: 0;
+  }
+
+  &[type="number"]::-webkit-inner-spin-button,
+  &[type="number"]::-webkit-outer-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
+
+  &:focus-visible {
+    outline-color: ${({ theme }) => theme.purple};
+  }
+`;
+
+export const StyledLabel = styled.label`
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+  color: ${({ theme }) => theme.smokeyGrey};
+  font-weight: 700;
+  text-transform: uppercase;
+  letter-spacing: 1.5px;
+  font-size: 13px;
 `;
