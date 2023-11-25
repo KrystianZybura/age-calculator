de687aa1ca9c1d2e81388674962e78c38d431a10
Krystian Żybura <Krystian12324444@gmail.com>
2023-11-25 12:44:18 +0100

    controled input values added

--- src/App/Form/Result/index.jsx
+++ src/App/Form/Result/index.jsx
@@ -1,20 +1,22 @@
 import { FancyResultText, Output, Wrapper } from "./styled";
 
-const Result = () => (
-  <Wrapper>
-    <Output>
-      <FancyResultText>- -</FancyResultText>
-      <span>Years</span>
-    </Output>
-    <Output>
-      <FancyResultText>- -</FancyResultText>
-      <span>Months</span>
-    </Output>
-    <Output>
-      <FancyResultText>- -</FancyResultText>
-      <span>Days</span>
-    </Output>
-  </Wrapper>
-);
+const Result = ({ day, month, year }) => {
+  return (
+    <Wrapper>
+      <Output>
+        <FancyResultText>- -</FancyResultText>
+        <span>Years</span>
+      </Output>
+      <Output>
+        <FancyResultText>- -</FancyResultText>
+        <span>Months</span>
+      </Output>
+      <Output>
+        <FancyResultText>- -</FancyResultText>
+        <span>Days</span>
+      </Output>
+    </Wrapper>
+  );
+};
 
 export default Result;
--- src/App/Form/index.jsx
+++ src/App/Form/index.jsx
@@ -10,31 +10,74 @@ import {
 
 import Arrow from "../assets/arrowIcon.svg?react";
 import Result from "./Result";
+import { useState } from "react";
 
-const Form = () => (
-  <StyledForm>
-    <InputsWrapper>
-      <StyledLabel htmlFor="day">
-        Day
-        <StyledInput name="day" type="number" placeholder="DD" autoFocus />
-      </StyledLabel>
-      <StyledLabel htmlFor="month">
-        Month
-        <StyledInput name="month" type="number" placeholder="MM" />
-      </StyledLabel>
-      <StyledLabel htmlFor="year">
-        Year
-        <StyledInput name="year" type="number" placeholder="YYYY" />
-      </StyledLabel>
-    </InputsWrapper>
-    <ButtonContainer>
-      <Line />
-      <Button>
-        <Arrow />
-      </Button>
-    </ButtonContainer>
-    <Result />
-  </StyledForm>
-);
+const Form = () => {
+  const [inputDate, setInputDate] = useState({
+    day: null,
+    month: null,
+    year: null,
+  });
 
+  return (
+    <StyledForm onSubmit={(event) => event.preventDefault()}>
+      <InputsWrapper>
+        <StyledLabel htmlFor="day">
+          Day
+          <StyledInput
+            onChange={({ target }) =>
+              setInputDate((inputDate) => ({ ...inputDate, day: target.value }))
+            }
+            value={inputDate.day ?? ""}
+            name="day"
+            type="number"
+            placeholder="DD"
+            autoFocus
+          />
+        </StyledLabel>
+        <StyledLabel htmlFor="month">
+          Month
+          <StyledInput
+            onChange={({ target }) =>
+              setInputDate((inputDate) => ({
+                ...inputDate,
+                month: target.value,
+              }))
+            }
+            value={inputDate.month ?? ""}
+            name="month"
+            type="number"
+            placeholder="MM"
+          />
+        </StyledLabel>
+        <StyledLabel htmlFor="year">
+          Year
+          <StyledInput
+            onChange={({ target }) =>
+              setInputDate((inputDate) => ({
+                ...inputDate,
+                year: target.value,
+              }))
+            }
+            value={inputDate.year ?? ""}
+            name="year"
+            type="number"
+            placeholder="YYYY"
+          />
+        </StyledLabel>
+      </InputsWrapper>
+      <ButtonContainer>
+        <Line />
+        <Button>
+          <Arrow />
+        </Button>
+      </ButtonContainer>
+      <Result
+        day={inputDate.day}
+        month={inputDate.month}
+        year={inputDate.year}
+      />
+    </StyledForm>
+  );
+};
 export default Form;
